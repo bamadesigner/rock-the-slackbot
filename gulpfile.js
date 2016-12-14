c0fab028e73436725393474f5c5a47912a372028
Rachel Carden <bamadesigner@gmail.com>
2016-12-13 20:23:56 -0800

    Adding PHPCS

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,48 @@
 var gulp = require('gulp');
 var minify = require('gulp-minify');
+var phpcs = require('gulp-phpcs');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 
-gulp.task('sass', function() {
-	gulp.src('css/admin-tools.scss')
+var to_watch = {
+	sass: ['css/admin-tools.scss'],
+	js: ['js/admin-tools.js'],
+	php: ['**/*.php','!vendor/**','!node_modules/**']
+};
+
+// Compile the SASS
+gulp.task('sass',function() {
+	gulp.src(to_watch.sass)
 		.pipe(sass({outputStyle:'compressed'}))
 		.pipe(gulp.dest('css'));
 });
 
-gulp.task('compress', function() {
-  gulp.src('js/admin-tools.js')
+// Minify the JS
+gulp.task('js',function() {
+  gulp.src(to_watch.js)
 	  .pipe(minify())
 	  .pipe(gulp.dest('js'))
 });
 
-gulp.task('default', ['sass','compress'], function() {
-	gulp.watch(['css/admin-tools.scss'],['sass']);
-	gulp.watch(['js/admin-tools.js'],['compress']);
+// Compile our assets
+gulp.task('compile',['sass','js']);
+
+// Check our PHP
+gulp.task('php',function() {
+	gulp.src(to_watch.php)
+		.pipe(phpcs({
+			bin: 'vendor/bin/phpcs',
+			standard: 'WordPress-Core'
+		}))
+		.pipe(phpcs.reporter('log'));
 });
+
+// Watch the files
+gulp.task('watch',function() {
+    gulp.watch(to_watch.sass,['sass']);
+	gulp.watch(to_watch.js,['js']);
+	gulp.watch(to_watch.php,['php']);
+});
+
+// Our default tasks
+gulp.task('default',['compile','php']);
